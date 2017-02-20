f0908c9a9b1545d814f5511532b0e14a9061bb3a
lucas8 <luc.linux@mailoo.org>
2017-02-20 12:48:27 +0100

    Begin code generation

--- Imple/src/Type.cpp
+++ Imple/src/Type.cpp
@@ -6,6 +6,9 @@ using namespace std;
 ostream& operator << (ostream&out,Type* type){
     return type->print(out);
 }
+std::string Type::name() const {
+    return _name;
+}
 
 size_t SumType::getSize(){
     size_t res = 0;
@@ -24,11 +27,6 @@ size_t ProdType::getSize(){
 }
 
 
-BasicType::BasicType(std::string name,size_t size)
-    :_name(name),_size(size){
-
-}
-
 std::ostream& SumType::print(std::ostream& out){
     out << "(";
     for(auto p : _types){
@@ -46,3 +44,66 @@ std::ostream& ProdType::print(std::ostream& out){
 std::ostream& BasicType::print(std::ostream& out){
     return out << _name;
 }
+
+std::ostream& SumType::codegen(std::ostream& out){
+    /* Type definition */
+}
+std::ostream& ProdType::codegen(std::ostream& out){
+    /* Type definition */
+    out << "struct " << _name << "_t {" << endl;
+    for(size_t i = 0; i < _types.size(); ++i) {
+        out << "    " << _types[i]->name() << "_t* m" << i << ";" << endl;
+    }
+    out << "};" << endl;
+
+    /* Building */
+    out << _name << "_t* build_" << _name << "(";
+    for(size_t i = 0; i < _types.size(); ++i) {
+        out << _types[i]->name() << "_t* m" << i << "," << endl;
+    }
+    out << ") {" << endl;
+    out << "    " << _name << "_t* bd = new " << _name << "_t;" << endl;
+    for(size_t i = 0; i < _types.size(); ++i) {
+        out << "    bd->m" << i << " = m" << i << ";" << endl;
+    }
+    out << "    return bd;" << endl;
+    out << "}" << endl;
+    out << endl;
+
+    /* Matching */
+    out << "template <typename T>" << endl;
+    out << "T match_" << _name << "(" << _name << "_t* v, const std::function<T("
+        << _types[0]->name() << "_t*";
+    for(size_t i = 1; i < _types.size(); ++i) {
+        out << ", " << _types[i]->name() << "_t*";
+    }
+    out << ")>& f) {" << endl;
+    out << "    return f(";
+    for(size_t i = 0; i < _types.size(); ++i) {
+        out << (i == 0 ? "" : ", ") << "v->m" << i;
+    }
+    out << ");" << endl;
+    out << "}" << endl;
+    return out;
+}
+
+std::ostream& BasicType::codegen(std::ostream& out){
+    /* Type declaration */
+    /*
+    out << "typedef " << _name << " " << _name << "_t;" << endl;
+    out << endl;
+    */
+    /* Building */
+    out << _name << "_t* build_" << _name << "(" << _name << " v) {" << endl;
+    out << "    " << _name << "_t bd = new " << _name << "_t(v);" << endl;
+    out << "    " << "retun bd;" << endl;
+    out << "}" << endl;
+    out << endl;
+    /* Matching */
+    out << "template <typename T>" << endl;
+    out << "T match_" << _name << "(" << _name << "_t* v, const std::function<T(" << _name << ")>& f) {" << endl;
+    out << "    return f(*v);" << endl;
+    out << "}" << endl;
+    return out;
+}
+
--- Imple/src/Type.h
+++ Imple/src/Type.h
@@ -8,35 +8,46 @@
 #include <iostream>
 
 class Type {
+protected:
+    std::string _name;
 
 public :
+    explicit Type(const std::string& name) : _name(name) { }
     virtual size_t getSize() = 0;
     virtual std::ostream& print(std::ostream& out) = 0;
+    virtual std::ostream& codegen(std::ostream& out) = 0;
+    std::string name() const;
 };
 
 class SumType : public Type {
     std::map<std::string,Type*> _types;
 public :
-    explicit SumType (const std::map<std::string,Type*>& types) : _types(types){}
+    explicit SumType (const std::string& name, const std::map<std::string,Type*>& types)
+        : Type(name), _types(types) {}
     size_t getSize();
     std::ostream& print(std::ostream& out);
+    std::ostream& codegen(std::ostream& out);
 };
 
 class ProdType : public Type {
     std::vector<Type*> _types;
 public:
-    explicit ProdType (const std::vector<Type*>& types) : _types(types){}
+    explicit ProdType (const std::string& name, const std::vector<Type*>& types)
+        : Type(name), _types(types) {}
     size_t getSize();
     std::ostream& print(std::ostream& out);
+    std::ostream& codegen(std::ostream& out);
 };
 
 class BasicType : public Type {
-    std::string _name;
+    /* _name must be the name of a native c++ type */
     size_t _size;
 public :
-    BasicType(std::string name,size_t size);
+    explicit BasicType(const std::string& name,size_t size)
+        : Type(name), _size(size) {}
     inline size_t getSize(){return _size;}
     std::ostream& print(std::ostream& out);
+    std::ostream& codegen(std::ostream& out);
 };
 
 
