da8919ada3b516012d10c003b05df0b729ff7579
lucas8 <luc.linux@mailoo.org>
2017-03-10 16:29:49 +0100

    Add test code

---
+++ Imple/test/main.cpp
@@ -0,0 +1,62 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+using namespace std;
+
+#include "tree.cpp"
+
+ostream& show(ostream& out, leaf_t* lf);
+ostream& show(ostream& out, node_t* nd);
+ostream& show(ostream& out, tree_t* tr);
+
+ostream& show(ostream& out, leaf_t* lf) {
+    leaf_t::match<void>(lf, [&] (int i) { out << i; });
+    return out;
+}
+
+ostream& show(ostream& out, node_t* nd) {
+    node_t::match<void>(nd, [&] (tree_t* t1, tree_t *t2) {
+            out << "(";
+            show(out,t1);
+            out << ") (";
+            show(out,t2);
+            out << ")";
+            });
+    return out;
+}
+
+ostream& show(ostream& out, tree_t* tr) {
+    tree_t::match<void>(tr,
+            [&] (node_t* nd) { show(out, nd); },
+            [&] (leaf_t* lf) { show(out, lf); });
+    return out;
+}
+
+int main() {
+    int input;
+    vector<tree_t*> pile;
+
+    while(cin >> input) {
+        if(input != -42) {
+            pile.push_back(tree_t::build_leaf(leaf_t::build(input)));
+        } else if(pile.size() >= 2) {
+            tree_t* t1 = pile.back(); pile.pop_back();
+            tree_t* t2 = pile.back(); pile.pop_back();
+            pile.push_back(tree_t::build_node(node_t::build(t1, t2)));
+        } else {
+            cerr << "Invalid code" << endl;
+            return 1;
+        }
+
+        for(size_t i = 0; i < pile.size(); ++i) {
+            cout << i << ". ";
+            show(cout, pile[i]);
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
+
