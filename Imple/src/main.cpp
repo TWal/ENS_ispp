462ee7dbe0af5f457e807bc85fa4930d3d337af4
Théophile Wallez <theophile@wallez.net>
2017-03-24 14:30:48 +0100

    Be able to define more types than "tree"

--- Imple/src/main.cpp
+++ Imple/src/main.cpp
@@ -9,32 +9,22 @@
 using namespace std;
 
 int main(){
-    /*auto& input = cin;
+    auto& input = cin;
     Scanner scan(&input,"test"); // load scanner
     pair<string,Type*> type;
     yy::parser parser(scan,type); //load parser
     parser.parse(); //parse
     type.second->print(cerr);
-    cerr << endl;*/
-
-    LogScanner lscan(&cin,"logTest");
-    vector<log::LogElem> res;
-    log::parser lparser(lscan,res);
-    lparser.parse();
-    for(auto l : res){
-        cout << l << endl;
-    }
+    cerr << endl;
 
 
-
-
-    /*map<string,Type*> env;
-    env["tree"] = type.second;
+    map<string,Type*> env;
+    env[type.first] = type.second;
     BasicCodegen gen(cout, env);
 
     std::vector<Type*> decls(1, type.second);
     gen.define(decls);
     gen.declare(decls);
-    gen.codegen(decls);*/
+    gen.codegen(decls);
     return 0;
 }
