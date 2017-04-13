b76da4336b872e1ce02e3acad9dd553f5ec25363
Théophile Wallez <theophile@wallez.net>
2017-04-13 16:32:36 +0200

    Update mergesort.cpp to the new interface

--- algos/mergesort.cpp
+++ algos/mergesort.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <assert.h>
+inline void assert(bool) {}
+#include <array>
 #include <iostream>
 using namespace std;
 #include "generated/LinkedList.cpp"
 
+
 void printList(list_t* l) {
     list_t::match<void>(l,
         [&](cons_t* cons) {
-            cons_t::match<void>(cons, [&](int x, list_t* xs) {
-                printf("%d ", x);
-                printList(xs);
-            });
+            printf("%d ", cons_t::get0(cons));
+            printList(cons_t::get1(cons));
         },
         [&](nil_t*) {
             printf("\n");
@@ -37,22 +39,22 @@ list_t* randomList(int n) {
 list_t* merge(list_t* l1, list_t* l2) {
     return list_t::match<list_t*>(l1,
         [&](cons_t* c) {
-            return cons_t::match<list_t*>(c, [&](int x1, list_t* x1s) {
-                return list_t::match<list_t*>(l2,
-                    [&](cons_t* c) {
-                        return cons_t::match<list_t*>(c, [&](int x2, list_t* x2s) {
-                            if(x1 < x2) {
-                                return cons(x1, merge(x1s, l2));
-                            } else {
-                                return cons(x2, merge(l1, x2s));
-                            }
-                        });
-                    },
-                    [&](nil_t*) {
-                        return l1;
+            int x1 = cons_t::get0(c);
+            list_t* x1s = cons_t::get1(c);
+            return list_t::match<list_t*>(l2,
+                [&](cons_t* c) {
+                    int x2 = cons_t::get0(c);
+                    list_t* x2s = cons_t::get1(c);
+                    if(x1 < x2) {
+                        return cons(x1, merge(x1s, l2));
+                    } else {
+                        return cons(x2, merge(l1, x2s));
                     }
-                );
-            });
+                },
+                [&](nil_t*) {
+                    return l1;
+                }
+            );
         },
         [&](nil_t*) {
             return l2;
@@ -63,19 +65,19 @@ list_t* merge(list_t* l1, list_t* l2) {
 pair<list_t*, list_t*> split(list_t* l) {
     return list_t::match<pair<list_t*, list_t*>>(l,
         [&](cons_t* c) {
-            return cons_t::match<pair<list_t*, list_t*>>(c, [&](int x1, list_t* lp) {
-                return list_t::match<pair<list_t*, list_t*>>(lp,
-                    [&](cons_t* c) {
-                        return cons_t::match<pair<list_t*,list_t*>>(c, [&](int x2, list_t* lpp) {
-                            pair<list_t*, list_t*> rec = split(lpp);
-                            return make_pair(cons(x1, rec.first), cons(x2, rec.second));
-                        });
-                    },
-                    [&](nil_t*) {
-                        return make_pair(cons(x1, nil()), nil());
-                    }
-                );
-            });
+            int x1 = cons_t::get0(c);
+            list_t* lp = cons_t::get1(c);
+            return list_t::match<pair<list_t*, list_t*>>(lp,
+                [&](cons_t* c) {
+                    int x2 = cons_t::get0(c);
+                    list_t* lpp = cons_t::get1(c);
+                    pair<list_t*, list_t*> rec = split(lpp);
+                    return make_pair(cons(x1, rec.first), cons(x2, rec.second));
+                },
+                [&](nil_t*) {
+                    return make_pair(cons(x1, nil()), nil());
+                }
+            );
         },
         [&](nil_t*) {
             return make_pair(nil(), nil());
@@ -86,9 +88,8 @@ pair<list_t*, list_t*> split(list_t* l) {
 size_t length(list_t* l) {
     return list_t::match<size_t>(l,
         [&](cons_t* cons) {
-            return cons_t::match<size_t>(cons, [&](int, list_t* xs) {
-                return 1 + length(xs);
-            });
+            list_t* xs = cons_t::get1(cons);
+            return 1 + length(xs);
         },
         [&](nil_t*) {
             return 0;
@@ -122,8 +123,10 @@ list_t* mergeSort(list_t* l) {
 #endif
 
 int main() {
+    init_blocks();
     srand(42);
-    list_t* l = randomList(1000*1000);
+    list_t* l = randomList(100*1000);
+    //list_t* l = randomList(1000*1000);
     //printList(l);
     mergeSort(l);
     //printList(mergeSort(l));
